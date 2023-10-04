f718bb5bef4748df54a225c81d1410f3e3462a1c
gardenkim <oyasumiee2@gmail.com>
2023-10-04 17:50:50 +0900

    🔨 Chore: Create a controller

---
+++ app/Http/Controllers/NoticeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoticeController extends Controller
+{
+    // 전체 공지 리스트
+    public function index()
+    {
+        //
+    }
+
+    // 공지 작성(관리자)
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // 특정 id의 공지 확인
+    public function show(string $id)
+    {
+        //
+    }
+
+    // 공지 업데이트(관리자)
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    // 공지 삭제(관리자)
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    // 전체 리뷰 리스트
+    public function index()
+    {
+        //
+    }
+
+    // 리뷰 작성
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // 특정 id의 리뷰 보기
+    public function show(string $id)
+    {
+        //
+    }
+
+    // 리뷰 업데이트
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    // 리뷰 삭제
+    public function destroy(string $id)
+    {
+        //
+    }
+}
