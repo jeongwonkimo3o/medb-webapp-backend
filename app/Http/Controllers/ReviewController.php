df4360932f9d6117211daa6f0b6b1e069818b964
gardenkim <oyasumiee2@gmail.com>
2023-10-09 21:38:38 +0900

    Feat: Implement the index logic for the ReviewController

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,14 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+
+    private $reviewModel;
+
+    public function __construct(Review $review)
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+        $this->reviewModel = $review;
+    }
+
     // 전체 리뷰 리스트
     public function index()
     {
-        //
+        $reviews = $this->reviewModel->with(['images', 'feedbacks'])
+            ->orderBy('created_at', 'desc') // 최신 날짜 순
+            ->paginate(10);
+
+        return response()->json(['reviews' => $reviews], 200);
     }
 
     // 리뷰 작성
