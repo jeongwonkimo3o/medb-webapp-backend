6f5bfaa46c7b4762f49730e88e75e5c98eb1463f
gardenkim <oyasumiee2@gmail.com>
2024-03-24 01:28:12 +0900

    Refactor: Partial modification of the notice logic

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -22,31 +22,44 @@ class NoticeController extends Controller
     public function index()
     {
         $notices = $this->noticeModel->orderBy('created_at', 'desc') // 최신 날짜 순
-        ->paginate(10);
+            ->paginate(10);
         return response()->json(['notices' => $notices, 'message' => 'The notices has been successfully retrieved'], 200);
     }
 
-    // 공지 작성(관리자)
+    // 공지 작성(관리자, is_admin == 1인 경우에만 가능)
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|max:255',
-            'content' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation failed',
-                'errors' => $validator->errors()
-            ], 422);
+        // 현재 인증된 사용자가 관리자인지 확인
+        $currentUser = Auth::user();
+        if ($currentUser->is_admin != 1) {
+            return response()->json(['message' => '권한이 없습니다.'], 403);
         }
 
-        $notice = $this->noticeModel;
-        $notice->fill($request->all());
-        $notice->user_id = Auth::id();
+        // 제목과 내용 검증
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string'
+        ]);
+
+        // 공지사항 생성 및 사용자 ID 할당
+        $notice = new Notice;
+        $notice->title = $validatedData['title'];
+        $notice->content = $validatedData['content'];
+        $notice->user_id = $currentUser->id; // 현재 인증된 사용자의 ID를 user_id로 설정
         $notice->save();
 
-        return response()->json(['notice' => $notice, 'message' => 'The notice has been successfully created'], 201);
+        // 응답 반환
+        return response()->json(['message' => '공지사항이 성공적으로 작성되었습니다.'], 201);
+    }
+
+    // 최근 한건의 공지 확인
+    public function recent()
+    {
+        $notice = $this->noticeModel->orderBy('created_at', 'desc')->first();
+        if (!$notice) {
+            return response()->json(['message' => '공지를 찾을 수 없습니다.'], 404);
+        }
+        return response()->json(['notice' => $notice, 'message' => '공지사항 조회가 완료되었습니다.'], 200);
     }
 
     // 특정 id의 공지 확인
